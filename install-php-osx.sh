e48139a7e889e4439138a64ed07589d4a0725b68
Anh Duong <anh.duong.ninka@gmail.com>
2015-09-28 23:25:50 +0300

    Add install php script for osx

---
+++ install-php-osx.sh
@@ -0,0 +1,5 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew install php56
